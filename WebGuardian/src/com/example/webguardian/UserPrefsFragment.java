b5c5ebc8a07c4c87b85f4bb4e1555f1ea19dd482
a.maryushin <a.maryushin@192.168.54.96>
2013-02-14 11:10:53 +0200

    add prefs

---
+++ WebGuardian/src/com/example/webguardian/UserPrefsFragment.java
@@ -0,0 +1,15 @@
+package com.example.webguardian;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class UserPrefsFragment extends PreferenceFragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+
+		// Load the preferences from an XML resource
+		addPreferencesFromResource(R.xml.preferences);
+	}
+}
