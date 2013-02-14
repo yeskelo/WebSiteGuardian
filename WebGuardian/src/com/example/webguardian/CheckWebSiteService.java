c723b98debe4b43edd0aba8be2bb223b3a7a0bf6
a.maryushin <a.maryushin@192.168.54.96>
2013-02-14 18:50:45 +0200

    added logic for testing url for online/offline

--- WebGuardian/src/com/example/webguardian/CheckWebSiteService.java
+++ WebGuardian/src/com/example/webguardian/CheckWebSiteService.java
@@ -1,13 +1,25 @@
 package com.example.webguardian;
 
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.IBinder;
 import android.util.Log;
 
 public class CheckWebSiteService extends Service {
 
-	static final String TAG="UpdateService";
+	static final String TAG = "UpdateService";
+
 	@Override
 	public IBinder onBind(Intent arg0) {
 		return null;
@@ -31,4 +43,31 @@ public class CheckWebSiteService extends Service {
 		super.onDestroy();
 	}
 
+	public int is_online(String url) {
+		// check network
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+		// if no network is available networkInfo will be null
+		// otherwise check if we are connected
+		if (networkInfo == null || !networkInfo.isConnected()) {
+			return 0;
+		}
+
+		// check if website is online
+		HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet(url);
+		HttpResponse response;
+		try {
+			response = client.execute(request);
+		} catch (ClientProtocolException e) {
+			return 1;
+		} catch (IOException e) {
+			return 1;
+		}
+		int stat_code = response.getStatusLine().getStatusCode();
+		if (stat_code != 200)
+			return 1;
+		else
+			return 2;
+	}
 }
